d0aec6df40d738f370023226f349f13984ddac9e
Simon David <sim.david90@gmail.com>
2011-10-11 18:41:53 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimdaApp::Application.config.secret_token = 'f8ee42b4367edf3de5b152a148403c63faedd387c7cef2483cc607482ace20d633924588e48b0e6706294162e567235f1f112b54ed5b45fee5280012cad83994'
